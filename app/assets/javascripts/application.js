f1ac806dfc3ef6da5796a80f3083ed032e8ebe66
Erinn Springer <erinnlspringer@gmail.com>
2018-05-08 18:14:06 +0200

    show hide works

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,90 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
+//= require jquery
+//= require plugin
+//= require jquery_ujs
+//= require bootstrap-sprockets
 //= require_tree .
+
+
+$(document).ready(function(){
+  $(".kerstin").hide();
+  $(".rosana").hide();
+  $(".helene").hide();
+  $(".liza").hide();
+  $(".helga").hide();
+  $(".home-link").addClass("selected");
+$(".kerstin-link").click(function(){
+  $(".kerstin").show();
+  $(".rosana").hide();
+  $(".helene").hide();
+  $(".liza").hide();
+  $(".helga").hide();
+  $(".kerstin-link").addClass("selected");
+  $(".rosana-link").removeClass("selected");
+  $(".helene-link").removeClass("selected");
+  $(".liza-link").removeClass("selected");
+  $(".helga-link").removeClass("selected");
+});
+$(".rosana-link").click(function(){
+  $(".kerstin").hide();
+  $(".rosana").show();
+  $(".helene").hide();
+  $(".liza").hide();
+  $(".helga").hide();
+  $(".kerstin-link").removeClass("selected");
+  $(".rosana-link").addClass("selected");
+  $(".helene-link").removeClass("selected");
+  $(".liza-link").removeClass("selected");
+  $(".helga-link").removeClass("selected");
+});
+$(".helene-link").click(function(){
+  $(".kerstin").hide();
+  $(".rosana").hide();
+  $(".helene").show();
+  $(".liza").hide();
+  $(".helga").hide();
+  $(".kerstin-link").removeClass("selected");
+  $(".rosana-link").removeClass("selected");
+  $(".helene-link").addClass("selected");
+  $(".liza-link").removeClass("selected");
+  $(".helga-link").removeClass("selected");
+});
+$(".liza-link").click(function(){
+  $(".kerstin").hide();
+  $(".rosana").hide();
+  $(".helene").hide();
+  $(".liza").show();
+  $(".helga").hide();
+  $(".kerstin-link").removeClass("selected");
+  $(".rosana-link").removeClass("selected");
+  $(".helene-link").removeClass("selected");
+  $(".liza-link").addClass("selected");
+  $(".helga-link").removeClass("selected");
+});
+$(".helga-link").click(function(){
+  $(".kerstin").hide();
+  $(".rosana").hide();
+  $(".helene").hide();
+  $(".liza").hide();
+  $(".helga").show();
+  $(".kerstin-link").removeClass("selected");
+  $(".rosana-link").removeClass("selected");
+  $(".helene-link").removeClass("selected");
+  $(".liza-link").removeClass("selected");
+  $(".helga-link").addClass("selected");
+});
+$(".home-link").click(function(){
+  $(".kerstin").hide();
+  $(".rosana").hide();
+  $(".helene").hide();
+  $(".liza").hide();
+  $(".helga").hide();
+  $(".kerstin-link").removeClass("selected");
+  $(".rosana-link").removeClass("selected");
+  $(".helene-link").removeClass("selected");
+  $(".liza-link").removeClass("selected");
+  $(".helga-link").removeClass("selected");
+});
+});
